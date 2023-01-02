8ad5984c125c69908390bcb8d12f35b55ab6c2ff
fsl4faisal <fsl4faisal@gmail.com>
2023-01-02 10:11:41 +0530

    Adding Section 5 init

---
+++ Section-5-in-28-minutes/src/main/java/com/in28minutes/jpa/hibernate/Section5/in8/minutes/Section5In28MinutesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate.Section5.in8.minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section5In28MinutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section5In28MinutesApplication.class, args);
+	}
+
+}
---
+++ Section-5-in-28-minutes/src/test/java/com/in28minutes/jpa/hibernate/Section5/in8/minutes/Section5In28MinutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate.Section5.in8.minutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section5In28MinutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
